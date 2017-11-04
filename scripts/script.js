bcd25a90306d0e9bc55f10cddd52c22cc85903e6
Tho Tran <cinnar12@gmail.com>
2017-11-03 23:35:23 -0400

    finished assignment

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,19 @@
-document.getElementById("thePhoto").onclick = change;
+var photo1 = document.getElementById ('photo1');
+
+photo1.addEventListener('mouseover', function() {
+  photo1.src = "images/img2.jpg";
+});
+
+photo1.addEventListener('mouseout', function() {
+  photo1.src = "images/img1.jpg";
+});
+
+var photo2 = document.getElementById ('photo2');
+
+photo2.addEventListener('mouseover', function() {
+  photo2.src = "images/img4.jpg";
+});
+
+photo2.addEventListener('mouseout', function() {
+  photo2.src = "images/img3.jpg";
+});
